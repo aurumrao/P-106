62751422a402673cd75d325df5724ccdfa908718
aurumrao <109160817+aurumrao@users.noreply.github.com>
2023-11-01 14:15:58 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -5,3 +5,31 @@ function startClassification(){
 function modelReady(){
     classifier.classify(gotResults);
 }
+
+function gotResults(error, results) {
+    if (error){
+        console.error(error);
+    } else{
+        console.log(results);
+        random_number_r = Math.floor(Math.random() * 255)+ 1;
+        random_number_g = Math.floor(Math.random() * 255)+ 1;
+        random_number_b = Math.floor(Math.random() * 255)+ 1;
+        
+        document.getElementById("result_label"). innerHTML = 'I can hear - ' + results[0].label;
+        document.getElementById("result_confidence"). innerHTML = 'Accuracy - ' + (results[0].confidence*100).toFixed(2) + '%';
+        document.getElementById("result_label").style.color = "rgb(" +random_number_r+","+random_number_g+","+random_number_b+")";
+        document.getElementById("result_confidence").style.color = "rgb(" +random_number_r+","+random_number_g+","+random_number_b+")";
+        
+        img  = document.getElementById('pet_image');
+       
+        if (results[0].label == "Barking"){
+            img.src = 'dog.jpg';
+        } else if (results[0].label == "Meowing"){
+            img.src = 'cat.jpg';
+        } else if (results[0].label == "Chirping"){
+            img.src = 'parrot.jpg';   
+        } else {
+            img.src = 'dog.jpg';
+        }
+    }
+}
